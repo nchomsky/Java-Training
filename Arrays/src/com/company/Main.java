4eee44a232016ffccac65c01b815b6ee93e7f640
Noah Chomsky <nb.chomsky@gmail.com>
2021-12-20 00:52:58 -0500

    Array creation

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+        double[] myDoubleArray = new double[10];
+        System.out.println(myIntArray[5]);
     }
 }
